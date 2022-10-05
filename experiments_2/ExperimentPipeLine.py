aa472be19b74c2da242b76ad96018774fd6e0bd6
mbaddar <mbaddar2@gmail.com>
2022-10-05 10:31:26 +0200

    torch ode start : simple euler

--- experiments_2/ExperimentPipeLine.py
+++ experiments_2/ExperimentPipeLine.py
@@ -13,21 +13,29 @@ from viz.plots import single_feature_plt, get_feature_history, multi_feature_plt
 from viz.plots import trajectory_plt
 
 
-class ExperimentSphere:
+class ExperimentPipeline:
     def __init__(self, data_dim, device):
+
+        self.test_data_loader = None
         self.tensor_diff_eq_trainer = None
         self.neural_ode_trainer = None
-        self.data_loader = None
+        self.train_data_loader = None
         self.data_dim = data_dim
         self.device = device
+        self.logger = logging.getLogger()
 
-    def load_data(self, num_points_inner=1000, num_points_outer=2000, batch_size=128):
+    def load_data(self, num_points_inner=2000, num_points_outer=4000, batch_size=128):
         data_concentric = ConcentricSphere(self.data_dim, inner_range=(0., .5), outer_range=(1., 1.5),
                                            num_points_inner=num_points_inner, num_points_outer=num_points_outer)
-        self.data_loader = DataLoader(data_concentric, batch_size=batch_size, shuffle=True)
+        N = len(data_concentric)
+        train_size = int(0.8 * N)
+        test_size = N - train_size
+        data_splits = torch.utils.data.dataset.random_split(dataset=data_concentric, lengths=[train_size, test_size])
+        self.train_data_loader = DataLoader(data_splits[0], batch_size=batch_size, shuffle=True)
+        self.test_data_loader = DataLoader(data_splits[1], batch_size=batch_size, shuffle=True)
 
     def viz_data(self, batch_size=1024, shuffle=True, viz_file='./data_centric.png'):
-        dataloader_viz = DataLoader(self.data_loader.dataset, batch_size=batch_size, shuffle=shuffle)
+        dataloader_viz = DataLoader(self.train_data_loader.dataset, batch_size=batch_size, shuffle=shuffle)
         for inputs, targets in dataloader_viz:
             break
         single_feature_plt(inputs, targets, save_fig=viz_file)
@@ -39,16 +47,16 @@ class ExperimentSphere:
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
         # Set up trainer
         self.neural_ode_trainer = Trainer(model, optimizer, self.device)
-        for inputs, targets in self.data_loader:
+        for inputs, targets in self.train_data_loader:
             break
         if visualize_features:
-            feature_history = get_feature_history(self.neural_ode_trainer, self.data_loader, inputs,
+            feature_history = get_feature_history(self.neural_ode_trainer, self.train_data_loader, inputs,
                                                   targets, num_epochs)
         else:
             # If we don't record feature evolution, simply train model
-            self.neural_ode_trainer.train(self.data_loader, num_epochs)
+            self.neural_ode_trainer.train(self.train_data_loader, num_epochs)
         multi_feature_plt(feature_history[::2], targets, save_fig='feature_history.png')
-        for small_inputs, small_targets in self.data_loader:
+        for small_inputs, small_targets in self.train_data_loader:
             break
         if visualize_training:
             trajectory_plt(model, small_inputs, small_targets, timesteps=10, save_fig='trajectory.png')
@@ -58,16 +66,20 @@ class ExperimentSphere:
             iteration_plt(histories=experiment_sphere.neural_ode_trainer.histories, y='nfe',
                           save_fig='neural_odes_nfes.png')
 
-    def train_tensor_ode(self, lr=1e-3, num_epochs=1000):
+    def train_tensor_ode(self, lr=1e-3, num_epochs=100):
         tensor_diff_eq_model = TensorDiffEq(input_dimensions=self.data_dim, output_dimensions=1, t_span=(0, 1),
                                             tensor_dimensions=[2, 2, 2])
         optimizer = torch.optim.Adam(params=tensor_diff_eq_model.parameters(), lr=lr)
         loss_func = torch.nn.SmoothL1Loss()
         self.tensor_diff_eq_trainer = TensorDiffEqTrainer(model=tensor_diff_eq_model, optimizer=optimizer,
-                                                          data_loader=self.data_loader, device=self.device,
-                                                          loss_func=loss_func, num_epochs=num_epochs, lr=1e-3, print_freq=10)
+                                                          train_data_loader=self.train_data_loader,
+                                                          test_data_loader=self.test_data_loader, device=self.device,
+                                                          loss_func=loss_func, num_epochs=num_epochs, lr=1e-3,
+                                                          print_freq=20)
 
         self.tensor_diff_eq_trainer.train(verbose=True)
+        avg_loss = self.tensor_diff_eq_trainer.evaluate()
+        self.logger.info(f'average loss = {avg_loss}')
 
 
 if __name__ == '__main__':
@@ -76,7 +88,7 @@ if __name__ == '__main__':
     data_dim = 2
     device = torch.device('cpu')
     #########
-    experiment_sphere = ExperimentSphere(data_dim=data_dim, device=device)
+    experiment_sphere = ExperimentPipeline(data_dim=data_dim, device=device)
     experiment_sphere.load_data()
     experiment_sphere.viz_data()
     # experiment_sphere.train_neural_ode()
