c89c9c191b1d4e850844f566376d9a781821a4f1
mbaddar <mbaddar2@gmail.com>
2023-02-05 11:05:46 +0100

    simple TT regression problem to test TT-ALS vs TT-Gradient Descent

---
+++ phd_experiments/tt_ode/simple_tt_reg_problem.py
@@ -0,0 +1,12 @@
+"""
+The purpose of this script is to experiment
+1- ALS training
+2- Gradient descent training ( as a base-line)
+for a simple sklearn regression problem
+Why? I want to set a several baselines
+1- TT-ODE trained by normal Backprop-Gradient-Descent
+2- TT-ODE trained by Adjoint-Sensitivity
+"""
+
+if __name__ == '__main__':
+    pass
